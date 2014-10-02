7a51ebd9ff3bcdd7b39fb16d1dc074303e339a67
Alex Lobunets <alexander.lobunets@gmail.com>
2014-10-02 17:47:54 +0200

    Added client component. Updated docs

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 This repository contains the following components:
 
- * Websocket server
- * Websocket client
+ * Websocket Server
+ * Websocket Client
 
-## Websocket Server component
+## Websocket Server Component
 
 Usage of Websocket server component:
 
@@ -43,3 +43,48 @@ runtime | Sending 'localhost:9999' to socket 'tcp://127.0.0.1:5000'
 ```
 
 Open http://www.websocket.org/echo.html and type in the following location `ws://localhost:9999/ws`. Then enter the following message `{"msg":"Hi!"}` and send it. You should receive exactly the same response back.
+
+## Websocket Client Component
+
+Usage of Websocket client component:
+
+```
+$ ./components/websocket/client
+Usage of ./components/websocket/client:
+  -debug=false: Enable debug mode
+  -json=false: Print component documentation in JSON
+  -port.in="": Component's input port endpoint
+  -port.options="": Component's options port endpoint
+  -port.out="": Component's output port endpoint
+```
+
+Example (Connecting to public websocket echo server):
+
+```
+# Configure client to connect to a public echo server
+'ws://echo.websocket.org:80' -> OPTIONS Client(websocket/client)
+
+# Output all received data on the screen
+Client OUT -> IN Log(core/console)
+
+# Configure ticker (sends timestamp every 3 seconds)
+'3s' -> INTERVAL Ticker(core/ticker)
+
+# Send ticker's timestamp to the client
+Ticker OUT -> IN Client
+```
+
+Running example:
+
+```
+...
+Ticker  | 1412264587
+Client  | Sending data from IN port to websocket...
+Client  | Writer: will send to websocket [49 52 49 50 50 54 52 53 56 55]
+Client  | Reader: received from websocket [49 52 49 50 50 54 52 53 56 55]
+Client  | Sending data from websocket to OUT port...
+Log     | IP: 1412264587
+...
+```
+
+As you can see above a tick `1412264587` is sent to Client, which sends it to Websocket server. The echo server responds back with the same message, which is then forwarded to Log component and shown in the terminal output.
---
+++ client/main.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"code.google.com/p/go.net/websocket"
+	"flag"
+	"fmt"
+	zmq "github.com/alecthomas/gozmq"
+	"github.com/cascades-fbp/cascades/components/utils"
+	"github.com/cascades-fbp/cascades/runtime"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var (
+	// Flags
+	optionsEndpoint = flag.String("port.options", "", "Component's options port endpoint")
+	inputEndpoint   = flag.String("port.in", "", "Component's input port endpoint")
+	outputEndpoint  = flag.String("port.out", "", "Component's output port endpoint")
+	jsonFlag        = flag.Bool("json", false, "Print component documentation in JSON")
+	debug           = flag.Bool("debug", false, "Enable debug mode")
+
+	// Internal
+	context                      *zmq.Context
+	optionsPort, inPort, outPort *zmq.Socket
+	err                          error
+)
+
+type Connection struct {
+	WS      *websocket.Conn
+	Send    chan []byte
+	Receive chan []byte
+}
+
+func NewConnection(wsConn *websocket.Conn) *Connection {
+	connection := &Connection{
+		WS:      wsConn,
+		Send:    make(chan []byte, 256),
+		Receive: make(chan []byte, 256),
+	}
+	return connection
+}
+
+func (self *Connection) Reader() {
+	for {
+		var data []byte
+		err := websocket.Message.Receive(self.WS, &data)
+		if err != nil {
+			log.Println("Reader: error receiving message")
+			continue
+		}
+		log.Println("Reader: received from websocket", data)
+		self.Receive <- data
+	}
+	self.WS.Close()
+}
+
+func (self *Connection) Writer() {
+	for data := range self.Send {
+		log.Println("Writer: will send to websocket", data)
+		err := websocket.Message.Send(self.WS, data)
+		if err != nil {
+			break
+		}
+	}
+	self.WS.Close()
+}
+
+func (self *Connection) Close() {
+	close(self.Send)
+	close(self.Receive)
+	self.WS.Close()
+}
+
+func validateArgs() {
+	if *optionsEndpoint == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+	if *inputEndpoint == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+	if *outputEndpoint == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+}
+
+func openPorts() {
+	context, err = zmq.NewContext()
+	utils.AssertError(err)
+
+	optionsPort, err = utils.CreateInputPort(context, *optionsEndpoint)
+	utils.AssertError(err)
+
+	inPort, err = utils.CreateInputPort(context, *inputEndpoint)
+	utils.AssertError(err)
+}
+
+func closePorts() {
+	optionsPort.Close()
+	inPort.Close()
+	if outPort != nil {
+		outPort.Close()
+	}
+	context.Close()
+}
+
+func main() {
+	flag.Parse()
+
+	if *jsonFlag {
+		doc, _ := registryEntry.JSON()
+		fmt.Println(string(doc))
+		os.Exit(0)
+	}
+
+	log.SetFlags(0)
+	if *debug {
+		log.SetOutput(os.Stdout)
+	} else {
+		log.SetOutput(ioutil.Discard)
+	}
+
+	validateArgs()
+
+	openPorts()
+	defer closePorts()
+
+	utils.HandleInterruption()
+
+	// Wait for the configuration on the options port
+	var connectString string
+	for {
+		ip, err := optionsPort.RecvMultipart(0)
+		if err != nil {
+			log.Println("Error receiving IP:", err.Error())
+			continue
+		}
+		if !runtime.IsValidIP(ip) || !runtime.IsPacket(ip) {
+			continue
+		}
+		connectString = string(ip[1])
+		break
+	}
+	optionsPort.Close()
+
+	// Establish WS connection
+	hostname, _ := os.Hostname()
+	origin := fmt.Sprintf("http://%s", hostname)
+	ws, err := websocket.Dial(connectString, "", origin)
+	utils.AssertError(err)
+
+	connection := NewConnection(ws)
+	defer connection.Close()
+
+	go connection.Reader()
+	go connection.Writer()
+	go func() {
+		outPort, err = utils.CreateOutputPort(context, *outputEndpoint)
+		utils.AssertError(err)
+		for data := range connection.Receive {
+			log.Println("Sending data from websocket to OUT port...")
+			outPort.SendMultipart(runtime.NewPacket(data), 0)
+		}
+	}()
+	log.Println("Started")
+
+	// Listen to packets from IN port
+	for {
+		ip, err := inPort.RecvMultipart(0)
+		if err != nil {
+			log.Println("Error receiving message:", err.Error())
+			continue
+		}
+		if !runtime.IsValidIP(ip) {
+			continue
+		}
+
+		log.Println("Sending data from IN port to websocket...")
+		connection.Send <- ip[1]
+	}
+}
